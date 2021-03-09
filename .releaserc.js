250616ad591142d7ea9fc04c405277196c333774
Alper Numan Genc <alper.numan.genc@accenture.com>
2021-03-09 15:54:32 +0100

    chore: semantic-release configuration

--- .releaserc.js
+++ .releaserc.js
@@ -7,8 +7,7 @@ module.exports = {
         // https://github.com/semantic-release/commit-analyzer#releaserules
         releaseRules: [
           {
-            scope:
-              'no-release',
+            scope: 'no-release',
             release: false,
           },
           {
@@ -31,20 +30,13 @@ module.exports = {
         ],
       },
     ],
-    [
-      '@semantic-release/release-notes-generator',
-      { preset: 'angular' },
-    ],
+    ['@semantic-release/release-notes-generator', { preset: 'angular' }],
     '@semantic-release/changelog',
     '@semantic-release/npm',
     [
       '@semantic-release/git',
       {
-        assets: [
-          'CHANGELOG.md',
-          'package.json',
-          '.releaserc.js',
-        ],
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json', '.releaserc.js'],
       },
     ],
     '@semantic-release/github',
